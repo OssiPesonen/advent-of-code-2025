693d9cf8b33b48f7259ad7e02ae39a21c214e780
Ossi Pesonen <ossi@ossipesonen.fi>
2025-12-07 21:55:27 +0200

    feat(app): Day 3 part 2

---
+++ Day3/Day3Part1.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2025.Day3;
+
+public class Day3Part1 : IPuzzle
+{
+    private static IEnumerable<string> Lines => File.ReadLines("input.txt");
+    
+    public Task RunAsync()
+    {
+        var result = 0;
+        foreach (var line in Lines)
+        {
+            var largestNumber = 0;
+            for (var i = 0; i < line.Length; i++)
+            {
+                var firstNumber = line[i];
+                for (var k = 1 + i; k < line.Length; k++) {
+                    var secondNumber = line[k];
+                    var largestBatteries = int.Parse(firstNumber + secondNumber.ToString());
+                    if (largestBatteries > largestNumber) largestNumber = largestBatteries;
+                }
+            }
+            result += largestNumber;
+        }
+        
+        Console.WriteLine("Answer is " + result);
+        return Task.CompletedTask;
+    }
+}
---
+++ Day3/Day3Part2.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode2025.Day3;
+
+public class Day3Part2 : IPuzzle
+{
+    private static IEnumerable<string> Lines => File.ReadLines("input.txt");
+
+    public Task RunAsync()
+    {
+        long result = 0;
+        const int maxLen = 12;
+        foreach (var line in Lines)
+        {
+            var toRemove = line.Length - maxLen;
+            var stack = new Stack<char>();
+
+            // Go through each digit and keep the highest one on top of the stack
+            foreach (var c in line)
+            {
+                while (stack.Count > 0 && toRemove > 0 && stack.Peek() < c)
+                {
+                    stack.Pop();
+                    toRemove--;
+                }
+
+                stack.Push(c);
+            }
+            
+            var number = stack.Reverse().Take(maxLen);
+            result += long.Parse(string.Join("", number));
+        }
+        
+        Console.WriteLine("Answer is " + result);
+        return Task.CompletedTask;
+    }
+}

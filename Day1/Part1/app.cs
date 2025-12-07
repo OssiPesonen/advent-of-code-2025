6b32b411fabee54f568ba4ade558d0f69be7bf3c
Ossi Pesonen <ossi@ossipesonen.fi>
2025-12-07 11:55:16 +0200

    feat(app): Day 1 part 1 and solution file etc.

---
+++ Day1/Part1/app.cs
@@ -0,0 +1,34 @@
+﻿// app.cs
+
+const string input = "input.txt";
+if (!File.Exists(input))
+    throw new Exception(
+        $"Input file was not found at project root. Please create input.txt and add your puzzle input there.");
+var lines = File.ReadLines(input);
+
+const int right = 1;
+const int left = -1;
+const int startingPoint = 50;
+
+var numberOfTimesPastZero = 0;
+var positionOnDial = startingPoint;
+
+foreach (var line in lines)
+{
+    var dir = line.Substring(0, 1);
+    var steps = Int64.Parse(line.Remove(0, 1));
+    for (var i = 0; i < steps; i++)
+    {
+        positionOnDial += dir == "L" ? left : right;
+        positionOnDial = positionOnDial switch
+        {
+            -1 => 99,
+            100 => 0,
+            _ => positionOnDial
+        };
+    }
+    
+    if (positionOnDial == 0) numberOfTimesPastZero++;
+}
+
+Console.WriteLine("Answer is " + numberOfTimesPastZero + "");
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of code 2025
 
 Trying to solve some of the puzzles in [Advent of Code, 2025](https://adventofcode.com/) if I find the time. This year using C#. 
+
+If you have [Taskfile](https://taskfile.dev) installed, just hit `task run:1:1` where the first number is the day and the second the part of the problem (1/2).
